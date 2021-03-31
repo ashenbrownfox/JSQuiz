0b8eb42aeffb3a019a0052b85f7c2d2bd1ac89b0
ashenbrownfox <newhoopsdynasty@gmail.com>
2021-03-31 16:51:48 -0400

    first commit

---
+++ quiz1.js
@@ -0,0 +1,14 @@
+const array = [
+ {x: "foo", y: 8},
+ {x: "bar", y: 3},
+ {x: "baz", y: 7}
+];
+
+const foo = array.reduce(
+    (a,b)=>({
+        ...a,
+        [b,x]: b.y,
+    }),
+    {}
+);
+console.log("test");
